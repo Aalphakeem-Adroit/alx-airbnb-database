ee5749f71eab69b5247973edc3c89f33a06727f6
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-10-29 07:12:55 +0100

    Fix: Checks for LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -38,6 +38,9 @@ LEFT JOIN
 ON 
     Property.property_id = Review.property_id;
 
+ORDER BY 
+    Property.property_id ASC;
+
 
 
 -- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
